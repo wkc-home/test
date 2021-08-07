175addd055016179dfb9e38bb92b21d8b836fa60
Kecheng <33301575+wkc-home@users.noreply.github.com>
2021-08-07 10:52:49 +0800

    在type.h中添加注释

--- BaseProject/Test1/type_define.h
+++ BaseProject/Test1/type_define.h
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+//宏值定义
 #define TRUE				1
 #define FALSE				0
-
+//类型定义
 typedef unsigned char		u8;
 typedef unsigned short		u16;
 typedef unsigned int		u32;
