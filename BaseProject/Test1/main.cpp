1b79c489b5a1ae10046de6786baae164a39f4108
Kecheng <33301575+wkc-home@users.noreply.github.com>
2021-08-07 10:46:27 +0800

    第一个HelloWorld程序

---
+++ BaseProject/Test1/main.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "type_define.h"
+
+void main()
+{
+	u8 u8Temp1;
+
+	u8Temp1 = TRUE;
+	if (TRUE == u8Temp1)
+	{
+		printf("Hello World !");
+	}
+	else
+	{
+		printf("Good Bye !");
+	}
+
+
+	return;
+}
---
+++ BaseProject/Test1/type_define.h
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define TRUE				1
+#define FALSE				0
+
+typedef unsigned char		u8;
+typedef unsigned short		u16;
+typedef unsigned int		u32;
+typedef unsigned long long	u64;
+
+typedef signed char			s8;
+typedef signed short		s16;
+typedef signed int			s32;
+typedef signed long long	s64;
+
+
+
+
